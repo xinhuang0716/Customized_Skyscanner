ee4f8420cb638bde23c2f2cea6c7ad7cbc6ffaf6
HSIN, HUANG <89914115+xinhuang0716@users.noreply.github.com>
2023-12-22 16:24:00 +0800

    Update and rename main.ipynb to main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+## import packages
+from utilities.airline import TigerQuery
+from utilities.optimal import OptimalTrip
+import json, warnings, pandas as pd
+warnings.filterwarnings("ignore")
+
+## airport code
+airport_dict = json.load(open("./config/airport.json"))
+
+## query
+df_tiger_go = pd.concat([TigerQuery(i, "2024-01-01", "2024-05-31") for i in list(airport_dict.keys())] + 
+                        [TigerQuery(i, "2024-06-01", "2024-10-31") for i in list(airport_dict.keys())])
+df_tiger_back = pd.concat([TigerQuery(i, "2024-01-01", "2024-05-31", False) for i in list(airport_dict.keys())] + 
+                          [TigerQuery(i, "2024-06-01", "2024-10-31", False) for i in list(airport_dict.keys())])
+
+## data preprocessing
+df = pd.concat([df_tiger_go, df_tiger_back])
+
+## output
+df.to_csv("./data/result_tiger.csv", index = False, encoding = "utf-8-sig")
+pd.concat([OptimalTrip(i) for i in range(2, 16)]).to_csv("./data/optimal_trip_tiger.csv", index = False, encoding = "utf-8-sig")
